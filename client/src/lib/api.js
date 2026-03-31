a91a6a76f1b2c6d8893a53da3a73c37ad86f0bc6
suganthi2306 <90704100+suganthi2306@users.noreply.github.com>
2026-03-31 23:38:40 +0530

    Fix API base URL for production

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -8,6 +8,6 @@ export const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("sk-admin-token");
-  if (token) config.headers.Authorization = `Bearer token`;
+  if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
